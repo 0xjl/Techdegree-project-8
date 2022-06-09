0c4a7b597fe3d8c321667fcafc3f23b3062d24fa
jcryptodev <jdl.lourens@gmail.com>
2022-06-09 20:40:13 +0200

    added js psudocode and global variables

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const url = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
+const main = document.getElementById("main");
+let employees = [];
+
+/// Fetch
+
+/// generateCard function
