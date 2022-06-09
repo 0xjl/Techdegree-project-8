4af1a32435a114972b849e2c855938fcab32b390
jcryptodev <jdl.lourens@gmail.com>
2022-06-09 23:19:30 +0200

    made the primitive version modal

--- js/app.js
+++ js/app.js
@@ -2,35 +2,81 @@ const url = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
 const main = document.getElementById("main");
 const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
 let employees = [];
 
 /// fetch ///
 fetch(url)
   .then((res) => res.json())
   .then((res) => res.results)
-  .then(generateCards)
+  .then(displayEmployees)
   .catch((err) => console.log(err));
 
 /// generateCard function
-function generateCards(data) {
+function displayEmployees(employeeData) {
+  employees = employeeData;
+  // store the employee HTML as we create it
   let employeeHTML = "";
-  data.forEach((data, index) => {
-    let name = data.name;
-    let email = data.email;
-    let city = data.location.city;
-    let picture = data.picture;
+  // loop through each employee and create HTML markup
+  employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+    // template literals make this so much cleaner!
     employeeHTML += `
-    <a>
-    <div class="lightbox card" data-index="${index}">
-    <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
-    <h2 class="name">${name.first} ${name.last}</h2>
-    <p class="email">${email}</p>
-    <p class="address">${city}</p>
-    </div>
-    </div>
-    </a>
-    `;
+  <div class="card" data-index="${index}">
+  <img class="avatar" src="${picture.large}" />
+  <div class="text-container">
+  <h2 class="name">${name.first} ${name.last}</h2>
+  <p class="email">${email}</p>
+  <p class="address">${city}</p>
+  </div>
+  </div>
+  `;
   });
   gridContainer.innerHTML = employeeHTML;
+
+  //displayModal function
+  function displayModal(index) {
+    // use object destructuring make our template literal cleaner
+    let {
+      name,
+      dob,
+      phone,
+      email,
+      location: { city, street, state, postcode },
+      picture,
+    } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+  <img class="avatar" src="${picture.large}" />
+  <div class="text-container">
+  <h2 class="name">${name.first} ${name.last}</h2>
+  <p class="email">${email}</p>
+  <p class="address">${city}</p>
+  <hr />
+  <p>${phone}</p>
+  <p class="address">${street}, ${state} ${postcode}</p>
+  <p>Birthday:
+  ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+  </div>
+  `;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+  }
+
+  gridContainer.addEventListener("click", (e) => {
+    if (e.target !== gridContainer) {
+      const card = e.target.closest(".card");
+      const index = card.getAttribute("data-index");
+      displayModal(index);
+    }
+  });
+
+  modalClose.addEventListener("click", () => {
+    overlay.classList.add("hidden");
+  });
 }
