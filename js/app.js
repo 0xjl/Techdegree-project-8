67e6473792782439a859e7779848391b7d949f5c
0xjdl <jdl.lourens@gmail.com>
2023-09-30 14:23:03 +0200

    little refactoring

--- js/app.js
+++ js/app.js
@@ -22,17 +22,14 @@ function displayEmployees(employeeData) {
   employees = employeeData;
   let employeeHTML = '';
   employees.forEach((employee, index) => {
-    let name = employee.name;
-    let email = employee.email;
-    let city = employee.location.city;
-    let picture = employee.picture;
+    const {name,email,location,picture} = employee;
     employeeHTML += `
   <div class="card flex-1" data-index="${index}">
   <img class="avatar" src="${picture.large}" />
   <div class="text-container">
   <h2 class="name">${name.first} ${name.last}</h2>
   <p class="email">${email}</p>
-  <p class="address">${city}</p>
+  <p class="address">${location.city}</p>
   </div>
   </div>
   `;
